6d073922dfb64a568f63896dd9de595127689868
Edvard Thörnros <edvard.thornros@gmail.com>
2024-03-02 20:30:05 +0100

    Add all my other configuration files

---
+++ .config/nvim/init.lua
@@ -0,0 +1,372 @@
+function plugins()
+  local Plug = vim.fn["plug#"]
+  vim.call("plug#begin", "~/.config/nvim/plugged")
+  do
+    Plug("tpope/vim-fugitive")
+    -- Plug("tpope/vim-repeat")
+    -- Plug("tpope/vim-surround")
+    Plug("tpope/vim-obsession")
+    -- Plug("tpope/vim-rhubarb")
+
+    -- Language extensions
+    Plug("FredTheDino/sylt.vim")
+    Plug("elixir-editors/vim-elixir")
+    Plug("purescript-contrib/purescript-vim")
+    Plug("rhysd/rust-doc.vim")
+    Plug("rust-lang/rust.vim")
+    Plug("tikhomirov/vim-glsl")
+    Plug("alaviss/nim.nvim")
+    Plug("terrastruct/d2-vim")
+
+    -- To minimize distractions
+    Plug("junegunn/goyo.vim")
+
+    -- Making it look good
+    Plug("jeffkreeftmeijer/vim-dim")
+    Plug("sainnhe/everforest")
+    Plug("EdenEast/nightfox.nvim")
+    Plug("catppuccin/nvim", { as="catppuccin" })
+    Plug("nvim-lualine/lualine.nvim")
+
+    -- Core workflow
+    Plug("neovim/nvim-lspconfig")
+    Plug("sbdchd/neoformat")
+
+    Plug("nvim-lua/plenary.nvim") -- Needed by telescope
+    Plug("nvim-telescope/telescope.nvim")
+
+    -- Treesitter
+    -- Plug("nvim-treesitter/nvim-treesitter")
+    -- Plug("nvim-treesitter/highlight.lua")
+    -- -- Plug("nvim-treesitter/nvim-treesitter-refactor")
+    -- Plug('nvim-treesitter/playground')
+
+    -- Under review
+    Plug("rafcamlet/nvim-luapad")
+    -- Plug("lukas-reineke/indent-blankline.nvim")
+    -- Plug("stevearc/aerial.nvim")
+    Plug("mvllow/modes.nvim")
+
+    Plug("FredTheDino/ma-pur-ma.nvim")
+
+    -- On thin ice
+    -- Plug 'easymotion/vim-easymotion'
+
+    -- Candidates
+    -- https://github.com/dcampos/nvim-snippy
+  end
+  vim.call("plug#end")
+end
+
+plugins()
+
+-- Color scheme
+require("nightfox").setup({
+  options = {
+    -- Compiled file's destination location
+    transparent = false,    -- Disable setting background
+    terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+    dim_inactive = true,   -- Non focused panes set to alternative background
+    styles = {              -- Style to be applied to different syntax groups
+      comments = "italic",    -- Value is any valid attr-list value `:help attr-list`
+      conditionals = "NONE",
+      constants = "bold",
+      functions = "bold",
+      keywords = "NONE",
+      numbers = "NONE",
+      operators = "NONE",
+      strings = "NONE",
+      types = "NONE",
+      variables = "NONE",
+    },
+    inverse = {             -- Inverse highlight for different types
+      match_paren = false,
+      visual = false,
+      search = false,
+    },
+    modules = {             -- List of various plugins and additional options
+      -- ...
+    },
+  }
+})
+-- vim.cmd.colorscheme "dim"
+vim.cmd.colorscheme "catppuccin-mocha"
+
+require('lualine').setup {
+  options = {
+    icons_enabled = false,
+    --theme = 'auto',
+    -- component_separators = { left = '', right = ''},
+    -- section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'FugitiveHead', 'diff'},
+    lualine_c = { { 'filename', path = 1 } },
+    lualine_x = {'encoding', { 'fileformat', symbols = { unix = '', dos = 'dos',  mac = 'mac' } }, 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { { 'filename', path = 1 } },
+    lualine_x = {'encoding', { 'fileformat', symbols = { unix = '', dos = 'dos',  mac = 'mac' } }, 'filetype'},
+    lualine_y = {},
+    lualine_z = {'location'}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+
+require('telescope').setup({
+  defaults = {
+    layout_config = {
+      -- other layout configuration here
+    },
+    -- other defaults configuration here
+  },
+})
+
+-- Formatter configs
+-- vim.g.neoformat_enabled_purescript = {"purstidy"}
+vim.g.neoformat_enabled_haskell = {"ormolu"}
+vim.g.neoformat_enabled_lua = {"lua-format"}
+
+-- Vim options
+vim.o.number = true
+vim.o.mouse = ""
+vim.o.breakindent = true
+vim.o.linebreak = true
+vim.o.undofile = true
+
+-- vim.o.showbreak = "|"
+vim.o.wrap = true
+vim.o.inccommand = "split"
+vim.o.scrolloff = 7
+vim.o.tabstop = 2
+vim.o.shiftwidth = vim.o.tabstop
+vim.o.softtabstop = 0
+vim.o.expandtab = true
+
+local xclip_clipboard_name = "clipboard"  
+vim.g.clipboard = {
+  name = "goodxclip",
+  copy = {
+    ['+'] = {'xclip', '-quiet', '-i', '-selection', xclip_clipboard_name},
+    ['*'] = {'xclip', '-quiet', '-i', '-selection', xclip_clipboard_name},
+  },
+  paste = {
+    ['+'] = {'xclip', '-o', '-selection', xclip_clipboard_name},
+    ['*'] = {'xclip', '-o', '-selection', xclip_clipboard_name},
+  },
+  cache_enabled = 1,
+}
+
+vim.o.spell = true
+vim.o.spelllang = "en_us"
+
+vim.o.hlsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.updatetime = 150
+vim.o.termguicolors = true
+
+-- Custom keymap
+vim.api.nvim_set_keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+
+-- TODO[et]: Should this be in a callback from the language server?
+
+vim.api.nvim_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+vim.cmd([[
+  nnoremap <C-g> <cmd>Telescope live_grep search_dirs=["lib/","src/","tests/"]<CR>
+  nnoremap <C-f> <cmd>Telescope find_files<CR>
+  nnoremap <C-b> <cmd>Telescope buffers<CR>
+  nnoremap <C-n> <cmd>Telescope tags<CR>
+  nnoremap <C-e> <cmd>Telescope diagnostics<CR>
+  " nnoremap <C-o> <cmd>Telescope git_branches<CR>
+  nnoremap <C-SPACE> <cmd>lua require('telescope.builtin').grep_string({search = vim.fn.expand("<cword>"), search_dirs={"lib/", "src/", "tests/"}, word_match="-w"})<cr>
+  " nnoremap <C-q> <cmd>:Lines<CR>
+  nnoremap <C-h> <C-w>h
+  nnoremap <C-j> <C-w>j
+  nnoremap <C-k> <C-w>k
+  nnoremap <C-l> <C-w>l
+  nnoremap <C-'> <cmd>Telescope marks<CR>
+  nnoremap <C-w><C-n> <ESC>:vert new<CR>
+  nnoremap <C-w>m <ESC><C-w>_<C-w>|
+  nnoremap <C-s> :update<CR>
+  inoremap <C-s> <ESC>:update<CR>i
+  tnoremap <C-C> <C-\><C-n>
+  nnoremap <SPACE><SPACE> :lua vim.lsp.buf.hover()<CR>
+  nnoremap <SPACE>a :lua vim.lsp.buf.code_action()<CR>
+  nnoremap <SPACE>d :lua vim.lsp.buf.definition()<CR>
+  nnoremap <SPACE>u :lua vim.lsp.buf.references()<CR>
+  nnoremap <SPACE>c :lua vim.lsp.buf.incoming_calls()<CR>
+
+  let g:purescript_disable_indent = 1
+  let g:purescript_unicode_conceal_enable = 0
+  " Sick-ass editing in command mode? :D
+  " nnoremap : q:i
+  au TextYankPost * silent! lua vim.highlight.on_yank()
+
+  augroup fmt
+    autocmd!
+    autocmd BufWritePre *.purs undojoin | Neoformat
+  augroup END
+
+  augroup fmt
+    autocmd!
+    autocmd BufWritePost *.dot undojoin | !dot -Tpdf % > %.pdf
+  augroup END
+
+  autocmd bufwritepost ~/.config/kitty/kitty.conf :silent !kill -SIGUSR1 $(pgrep kitty) && notify-send "Kitty config updated"
+]])
+
+-- Highlight on yank
+vim.highlight.on_yank()
+
+-- LSP Configs
+local nvim_lsp = require("lspconfig")
+nvim_lsp.elixirls.setup {cmd = {"/home/ed/Apps/elixir-ls/language_server.sh"}}
+nvim_lsp.purescriptls.setup {
+  settings = {
+    purescript = {
+      addSpagoSources = true,
+      codegenTargets = { "erl" },
+      formatter = "purs-tidy"
+    }
+  },
+  flags = {debounce_text_changes = 50}
+}
+nvim_lsp.rust_analyzer.setup {}
+nvim_lsp.jedi_language_server.setup {}
+nvim_lsp.texlab.setup {}
+nvim_lsp.hls.setup {}
+nvim_lsp.elmls.setup {}
+
+-- Other configs
+-- require("trouble").setup {}
+
+require("telescope.actions")
+require("telescope").setup {pickers = {buffers = {sort_lastused = true}}}
+
+local mpm = require "ma-pur-ma"
+vim.keymap.set('n', '<SPACE>pe', mpm.extract_to_function)
+vim.keymap.set('v', '<SPACE>pe', mpm.extract_to_function)
+vim.keymap.set('n', '<SPACE>pi', mpm.toggle_export)
+vim.keymap.set('n', '<SPACE>pf', mpm.if_to_case)
+vim.keymap.set('n', '<SPACE>pc', mpm.fill_in_data_case)
+
+local terafox_pallet = require("nightfox.palette").load("terafox")
+-- Add fancy indentation syntax
+-- vim.cmd(string.format("highlight IndentBlanklineIndent1 guibg=%s gui=nocombine", terafox_pallet.bg2))
+-- vim.cmd(string.format("highlight IndentBlanklineIndent2 guibg=%s gui=nocombine", terafox_pallet.bg1))
+-- 
+-- require("indent_blankline").setup {
+--     space_char_blankline = " ",
+--     show_current_context = true,
+--     show_current_context_start = true,
+-- }
+-- require("indent_blankline").setup {
+--     char = "",
+--     show_current_context = true,
+--     show_current_context_start = true,
+--     char_highlight_list = {
+--         "IndentBlanklineIndent1",
+--         "IndentBlanklineIndent2",
+--     },
+--     space_char_highlight_list = {
+--         "IndentBlanklineIndent1",
+--         "IndentBlanklineIndent2",
+--     },
+--     show_trailing_blankline_indent = false,
+-- }
+
+-- require("indent_blankline").setup {
+--     char = "|",
+--     show_current_context = true,
+--     show_current_context_start = true,
+--     show_current_context_start = true,
+--     show_trailing_blankline_indent = true,
+-- }
+
+-- require"nvim-treesitter.configs".setup {
+--   ensure_installed = { "c", "lua", "rust", "cpp" },
+-- 
+--   -- Automatically install missing parsers when entering buffer
+--   auto_install = true,
+-- 
+--   -- List of parsers to ignore installing (for "all")
+--   ignore_install = { "javascript", "purescript" },
+-- 
+--   highlight = {
+--     -- `false` will disable the whole extension
+--     enable = true,
+-- 
+--     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+--     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+--     -- Using this option may slow down your editor, and you may see some duplicate highlights.
+--     -- Instead of true it can also be a list of languages
+--     additional_vim_regex_highlighting = false,
+--   },
+-- 
+--   refactor = {
+--     highlight_definitions = {
+--       enable = true,
+--       -- Set to false if you have an `updatetime` of ~100.
+--       clear_on_cursor_move = true,
+--     },
+--     highlight_current_scope = { enable = false },
+--   },
+-- 
+--   playground = {
+--     enable = true,
+--     disable = {},
+--     updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+--     persist_queries = false, -- Whether the query persists across vim sessions
+--     keybindings = {
+--       toggle_query_editor = 'o',
+--       toggle_hl_groups = 'i',
+--       toggle_injected_languages = 't',
+--       toggle_anonymous_nodes = 'a',
+--       toggle_language_display = 'I',
+--       focus_language = 'f',
+--       unfocus_language = 'F',
+--       update = 'R',
+--       goto_node = '<cr>',
+--       show_help = '?',
+--     },
+--   },
+-- }
+
+-- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+-- parser_config.purescript = {
+--     install_info = {
+--       url = "https://github.com/Maskhjarna/tree-sitter-purescript",
+--       files = {"src/parser.c", "src/scanner.c"}, 
+--       branch = "main",
+--     },
+--     filetype = "purescript", -- if filetype does not match the parser name
+-- }
+-- vim.treesitter.language.register('purescript', 'mask_purescript')
---
+++ .zshrc
@@ -0,0 +1,90 @@
+# Use powerline
+USE_POWERLINE="false"
+# Has weird character width
+# Example:
+#    is not a diamond
+HAS_WIDECHARS="false"
+# Source manjaro-zsh-configuration
+if [[ -e /usr/share/zsh/manjaro-zsh-config ]]; then
+  source /usr/share/zsh/manjaro-zsh-config
+fi
+# Use manjaro zsh prompt
+# if [[ -e /usr/share/zsh/manjaro-zsh-prompt ]]; then
+#   source /usr/share/zsh/manjaro-zsh-prompt
+# fi
+
+if [[ -e /usr/share/fzf/key-bindings.zsh ]]; then
+  source /usr/share/fzf/key-bindings.zsh 
+fi
+if [[ -e /usr/share/fzf/completion.zsh ]]; then
+  source /usr/share/fzf/completion.zsh  
+fi
+
+# Custom config
+export BROWSER=$(which firefox)
+export EDITOR=$(which nvim)
+export VISUAL=$EDITOR
+
+# Alias
+alias reboot="sudo systemctl reboot"
+alias poweroff="sudo systemctl poweroff"
+alias hibernate="echo 'Going to bed' && sudo systemctl hibernate"
+alias suspend="echo 'Going to bed' && sudo systemctl suspend"
+
+alias :q="exit"
+alias vi='nvim'
+alias vim='nvim'
+alias edit='nvim $(fzf)'
+alias view='zathura'
+alias pomac='pamac'
+alias capslock='xdotool key Caps_Lock'
+alias keys='setxkbmap se -option caps:escape nodeadkeys'
+
+# Just throw in the keyconfig here
+
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey "^o" edit-command-line
+
+# Prompt
+
+show_git_status() {
+    IS_GIT=$( if [[ -d .git ]]; then echo "GIT"; fi )
+    GIT=""
+    if ! [[ -z $IS_GIT ]]; then
+        GIT_BRANCH=$( git rev-parse --abbrev-ref HEAD )
+
+        $(git diff --no-ext-diff --quiet)
+        if [[ $? -eq 0 ]]; then
+            GIT="%f[%B$GIT_BRANCH%b]"
+        else
+            GIT="%f<%B%F{red}$GIT_BRANCH%f%b>"
+        fi
+    fi
+    echo -n -e "$GIT"
+}
+
+show_ssh() {
+    if [[ -n $SSH_CONNECTION ]]; then
+        echo -n -e "%f%B%F{red}##%f%b"
+    fi
+}
+
+prompt() {
+    EXIT=$?
+    if [[ $EXIT -ne 0 ]]; then
+        EXIT="%B%F{red}-$EXIT-%f%b"
+    else
+        EXIT=""
+    fi
+    
+    echo -n -e "$(show_ssh)%f $(show_git_status) %B%F{green}%2~%f%b $EXIT%B%F{green}::%b%f "
+}
+
+GPG_TTY=$(tty)
+export GPG_TTY
+export XDG_DATA_HOME=$HOME/.local/share
+
+export PATH=~/.local/bin:~/Apps:$PATH:
+
+[ -f "/home/ed/.ghcup/env" ] && source "/home/ed/.ghcup/env" # ghcup-env
