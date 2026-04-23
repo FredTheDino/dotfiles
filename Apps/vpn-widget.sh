7264719a2a3ca866c14014e75e813da3e6ff84ca
Edvard Thörnros <edvard.thornros@gmail.com>
2026-04-24 00:36:52 +0200

    Remove some old stuff

--- Apps/vpn-widget.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-nordvpn status | head -1 | python3 -c "print('<span fgcolor=\"#000\" bgcolor=\"' + (\"#e75a7c\" if \"Dis\" in input() else \"#8fb573\") + '\">  </span>')"
--- Apps/weather-widget.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -Ss 'https://wttr.in/Uppsala?m&format=3' | python3 -c "print(*input().split(':')[1].split()[::-1])"
