13cb8bf4f0396c708b0e5cb765cd591fbd458d07
Edvard Thörnros <edvard.thornros@gmail.com>
2026-04-24 00:19:51 +0200

    Configure fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,19 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 
+bind ctrl-o 'edit_command_buffer'
+
+set --global EDITOR 'nvim'
+set --global VISUAL 'nvim'
+set -gx PATH /home/ed/Apps/ $PATH
+
+alias cat=bat
+alias less=bat
+alias more=bat
+
 set -x MANPAGER "bat -plman"
 fzf_configure_bindings
+
+function fish_greeting
+    echo (set_color red)(echo " ><> ")(set_color green)Swim on(set_color blue)(echo " ><>")(set_color --reset)
+end
